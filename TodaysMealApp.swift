ba93f961eff9038d128ff8494e79905ac94da561
joeun <whdms1107@naver.com>
2023-02-10 13:07:42 +0900

    init

---
+++ TodaysMealApp.swift
@@ -0,0 +1,18 @@
+//
+//  TodaysMealApp.swift
+//  TodaysMeal
+//
+//  Created by joeun on 2023/02/10.
+//
+//
+
+import SwiftUI
+
+@main
+struct TodaysMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
